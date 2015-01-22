f69ed42dec3a75161f38f28b9dd82bdfecd1fe5d
Neas87 <neas87@yandex.ru>
2015-01-22 12:53:12 +0400

    ssss

---
+++ Script/Camer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camer : MonoBehaviour {
+	[SerializeField]
+	float Smoothness = 4; // скорость с которой будет поворачиваться камера, т.е. голова
+	[SerializeField]
+	Vector2 Sensitivity = new Vector2 (4,4);// скорость изменения новых координат
+	private Vector2 NewCoord;// в которое повернется камера
+	public Vector2 CurrentCoord;// текущее положение
+	[SerializeField]
+	Vector2 Limit = new Vector2 (-70, 80);// ограничение на поворот
+	private Vector2 vel;// хер знает что. но нужна для функции
+
+	void Update () {
+		NewCoord.x = Mathf.Clamp (NewCoord.x, Limit.x, Limit.y); // ограничение на поворот, что бы не делать сальто головой
+		NewCoord.x-= Input.GetAxis ("Mouse Y")* Sensitivity.x;
+		NewCoord.y+= Input.GetAxis ("Mouse X")* Sensitivity.y;
+		CurrentCoord.x = Mathf.SmoothDamp (CurrentCoord.x, NewCoord.x, ref vel.x, Smoothness/100); // для реалестичного и плавного поворота
+		CurrentCoord.y = Mathf.SmoothDamp (CurrentCoord.y, NewCoord.y, ref vel.y, Smoothness/100);
+		transform.rotation = Quaternion.Euler (CurrentCoord.x, CurrentCoord.y, 0);
+	}
+}
---
+++ Script/Move.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public Vector3 Speed;
+	private Camer cam;// ссылка на скрипт камеры.
+	private bool Jamp;
+   public GameObject Cube1;
+
+	void Start () {
+		cam = GameObject.FindWithTag ("MainCamera").GetComponent <Camer> ();
+	}
+	
+	void OnCollisionEnter (Collision col) // проверка находится ли перс на земле (куб1)
+	{
+		if (col.gameObject.name == "Cube1")
+						Jamp = false;
+		
+	}
+	void Update () {
+			transform.rotation = Quaternion.Euler (0, cam.CurrentCoord.y, 0);// привязываем камерук персу
+			transform.Translate (Vector3.forward * Input.GetAxis ("Vertical") * Speed.x * Time.deltaTime);
+			transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * Speed.y * Time.deltaTime);
+	
+	if (Input.GetButtonDown ("Jump")) {
+			if (Jamp == false) {
+				Jamp = true;
+				rigidbody.AddForce (0, Speed.z * 100, 0)	;
+
+			}
+	}
+	}
+}
+
+		
+
+	
+	 
+		 
+	 
